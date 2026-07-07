0f5d0b73c5869c7bd4178346bb59c091a5df9fae
Sampriti Mandal <helloitsmesampriti12345@gmail.com>
2026-07-07 18:07:24 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int lowest=prices[0];
+        int profit=0;
+        for (int i=1; i<prices.size();i++){
+            if (lowest>prices[i]){
+                lowest=prices[i];
+            }
+            else{
+                if (prices[i]-lowest>profit){
+                    profit=prices[i]-lowest;
+                }
+            }
+        }
+        return profit;   
+    }
+};
